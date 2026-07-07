e1bece256b52169c3ccc38b762ace915e5fc1582
ARGtiq <98221716+ARGtiq@users.noreply.github.com>
2026-07-07 10:11:10 +0300

    From INIT
    Перезагружены изначальные файлы

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='neurocatch-v8';
+const CACHE='neurocatch-v5';
 const ASSETS=['./','./neurocatch.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
